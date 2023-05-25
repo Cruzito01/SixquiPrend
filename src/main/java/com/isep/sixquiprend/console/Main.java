f12885aafe6f49f968b8b3a5fe665b94abfb258e
SherineBY <117665564+SherineBY@users.noreply.github.com>
2023-05-25 22:58:10 +0200

    debut de jeu

--- src/main/java/com/isep/sixquiprend/console/Main.java
+++ src/main/java/com/isep/sixquiprend/console/Main.java
@@ -1,4 +1,12 @@
 package com.isep.sixquiprend.console;
 
+import com.isep.sixquiprend.core.Sixquiprend;
+
 public class Main {
+    Sixquiprend sixquiprend;
+
+    public static void main(String[] args){
+        Sixquiprend sixquiprend=new Sixquiprend();
+        sixquiprend.Startofgame();
+    }
 }
--- src/main/java/com/isep/sixquiprend/core/Sixquiprend.java
+++ src/main/java/com/isep/sixquiprend/core/Sixquiprend.java
@@ -2,6 +2,7 @@ package com.isep.sixquiprend.core;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Sixquiprend {
 
@@ -13,5 +14,24 @@ public class Sixquiprend {
         this.players= new ArrayList<>();
         this.board= new Board();
         this.cardinhand= new ArrayList<>();
+    }
+
+    public void Startofgame(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("choose the number players ho who is play into 1 and 10");
+        int numPlayers=scanner.nextInt();
+
+        if (numPlayers<1 || numPlayers>10){
+            System.out.println("invalid player numbers");
+            return;
+        }
+        for (int i=0;i<numPlayers;i++){
+            System.out.println("enter the name of player"+(i+1) );
+            String playername=scanner.next();
+            Player player=new Player(playername);
+            players.add(player);
+        }
+        System.out.println("the game can begin!!!");
+
     }
 }
