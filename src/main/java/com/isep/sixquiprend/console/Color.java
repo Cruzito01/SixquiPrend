b3ce72cc4a54ef5ea2297dcc74a6e592f3e07e90
SherineBY <117665564+SherineBY@users.noreply.github.com>
2023-05-25 11:39:12 +0200

    création des carte

---
+++ src/main/java/com/isep/sixquiprend/console/Color.java
@@ -0,0 +1,5 @@
+package com.isep.sixquiprend.console;
+
+public class Color {
+
+}
--- src/main/java/com/isep/sixquiprend/core/Card.java
+++ src/main/java/com/isep/sixquiprend/core/Card.java
@@ -1,5 +1,9 @@
 package com.isep.sixquiprend.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Card {
     private int cardsnumber;
     private int headofbeef;
@@ -22,5 +26,29 @@ public class Card {
 
     public void setHeadofbeef(int headofbeef) {
         this.headofbeef = headofbeef;
+    }
+
+    public static List<Card> generatecards() {
+        List<Card> generatecards = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 104; i++) {
+            numbers.add(i);
+        }
+        Random random = new Random();
+        for (int i = 1; i <= 104; i++) {
+            int number = numbers.get(i);
+            int headofbeef = 0;
+            if (number % 10 == 5) {
+                headofbeef += 2;
+            } else if (number % 10 == 0) {
+                headofbeef += 3;
+            } else if (number % 11 == 0) {
+                headofbeef += 5;
+            }
+            Card card = new Card(number, headofbeef);
+            generatecards.add(card);
+        }
+
+        return generatecards;
     }
 }
