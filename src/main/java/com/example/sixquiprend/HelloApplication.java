27f8139e043d079583083653d0e70010b8e88e92
Alexandre Cruz <alex-cruz@hotmail.fr>
2023-05-29 14:08:34 +0200

    new

--- src/main/java/com/example/sixquiprend/HelloApplication.java
+++ src/main/java/com/example/sixquiprend/HelloApplication.java
@@ -8,10 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    Stage stage;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        this.stage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Page1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
