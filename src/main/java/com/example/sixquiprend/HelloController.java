589b7b6c5bd4be22acad65587ae7242fc1eef9e0
Alexandre Cruz <alex-cruz@hotmail.fr>
2023-05-31 20:14:26 +0200

    Page inscription faite

--- src/main/java/com/example/sixquiprend/HelloController.java
+++ src/main/java/com/example/sixquiprend/HelloController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -38,4 +39,61 @@ public class HelloController {
         stage.show();
     }
 
+    @FXML
+    private ListView<String> playersListView;
+
+    @FXML
+    private TextField nameInput;
+
+    public void handleAddPlayerButton(ActionEvent event) {
+        String playerName = nameInput.getText();
+        if (!playerName.isEmpty()) {
+            ObservableList<String> items = playersListView.getItems();
+            items.add(playerName);
+            nameInput.clear();
+        }
+    }
+
+    @FXML
+    private Button buttonRemoveLastPlayer;
+
+    @FXML
+    public void handleRemoveLastPlayerButton(ActionEvent event) {
+        ObservableList<String> items = playersListView.getItems();
+        if (!items.isEmpty()) {
+            int lastIndex = items.size() - 1;
+            items.remove(lastIndex);
+        }
+    }
+
+    @FXML
+    private Button buttonGoToBoard;
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    public void handleGoToBoardButton(ActionEvent event) throws IOException {
+        ObservableList<String> items = playersListView.getItems();
+        int numPlayers = items.size();
+
+        if (numPlayers >= 1 && numPlayers <= 10) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("PageBoard.fxml"));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.show();
+
+                // Fermer la fenêtre actuelle si nécessaire
+                Stage currentStage = (Stage) buttonGoToBoard.getScene().getWindow();
+                currentStage.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            errorLabel.setVisible(true);
+            // Afficher un message d'erreur ou une notification indiquant les conditions requises
+        }
+
+    }
 }
