1683903fc494938d9c71ebe4c999f58327b42e78
Alexandre Cruz <alex-cruz@hotmail.fr>
2023-06-01 23:32:36 +0200

    ai

--- src/main/java/com/isep/sixquiprend/core/Ai.java
+++ src/main/java/com/isep/sixquiprend/core/Ai.java
@@ -1,4 +1,24 @@
 package com.isep.sixquiprend.core;
 
+import java.util.List;
+import java.util.Random;
+
 public class Ai {
+    private Random random;
+
+    public Ai() {
+        random = new Random();
+    }
+
+    public int chooseCard(List<Integer> availableCards) {
+        // Choix aléatoire d'une carte parmi celles disponibles
+        int randomIndex = random.nextInt(availableCards.size());
+        return availableCards.get(randomIndex);
+    }
+
+    public int chooseRow(List<Integer> cardOptions) {
+        // Choix aléatoire d'une rangée parmi celles disponibles pour une carte donnée
+        int randomIndex = random.nextInt(cardOptions.size());
+        return cardOptions.get(randomIndex);
+    }
 }
